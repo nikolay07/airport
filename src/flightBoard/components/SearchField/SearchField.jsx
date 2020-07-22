8fe2c6339d7756025a583027e89aa69013720bcf
nikolay07 <nikolay0701@ukr.net>
2020-07-22 21:30:01 +0300

    fixed

--- src/flightBoard/components/SearchField/SearchField.jsx
+++ src/flightBoard/components/SearchField/SearchField.jsx
@@ -1,52 +1,32 @@
 import React, { useState } from "react";
-import { connect } from "react-redux";
-import { useLocation, useHistory } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
-import qs from "qs";
+import { connect } from "react-redux";
 import { dateSelector } from "../../flights.selectors";
 
 function SearchField({ date }) {
-  const [inputValue, setInputValue] = useState("");
   const location = useLocation();
-  const history = useHistory();
-
-  const handleSearch = (event) => {
-    event.preventDefault();
-    let dataQuery = {
-      search: inputValue,
-      date,
-    };
-    let pathname = "";
-    if (location.pathname === "/") {
-      pathname = "/departures?";
-    } else {
-      dataQuery = {
-        ...dataQuery,
-        ...qs.parse(location.search, { ignoreQueryPrefix: true }),
-        search: inputValue,
-      };
-      pathname = location.pathname + "?";
-    }
-    const queryString = qs.stringify(dataQuery);
-    history.push(`${pathname}${queryString}`);
-  };
+  const [flightID, setflightID] = useState("");
 
   return (
     <div className="search-field">
       <h2 className="search-field__title">Search flight</h2>
-      <form className="search-field__form" onSubmit={handleSearch}>
+      <div className="search-field__form">
         <i className="material-icons search-field__icon">search</i>
         <input
-          className="search-field__input"
           type="text"
+          className="search-field__input"
           placeholder="Airline, destination or flight #"
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
+          value={flightID}
+          onChange={(event) => setflightID(event.target.value)}
         />
-        <button className="search-field__btn" type="submit">
-          Search
-        </button>
-      </form>
+
+        <Link to={`${location.pathname}?search=${flightID}&date=${date}`}>
+          <button className="search-field__btn" type="submit">
+            SEARCH
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
